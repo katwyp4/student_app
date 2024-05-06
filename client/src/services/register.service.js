cda42131601d060900f7607f29a04695d87852c8
245965 <245965@edu.p.lodz.pl>
2024-05-06 18:21:00 +0200

    Przygotowanie aplikacji klienta - frontend architektura

---
+++ client/src/services/register.service.js
@@ -0,0 +1,11 @@
+import { API_URL } from "./api.service";
+import axios from 'axios';
+
+export const signup = async (name, surname, email, password) => {
+    const response = await axios.post(`${API_URL}/auth/signup`, { name, surname, email, password });
+    localStorage.setItem('email', response.data.email);
+    localStorage.setItem('token', response.data.token);
+    localStorage.setItem('name', response.data.name);
+    localStorage.setItem('surname', response.data.surname);
+    return true;
+};
