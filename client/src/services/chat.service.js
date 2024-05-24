3647f3eea19005a3cf31efb20e760b15d336356c
245965 <245965@edu.p.lodz.pl>
2024-05-24 20:04:46 +0200

    W ramach tego commita  zostało wykonane : Dodanie serwisu odpowiedzialnego za komunikacje z backendem (/api/chat) Obsługa żądania GET i POST na ścieżce /api/chat po stronie serwera Obsługa awatarów w komponencie Chat

--- client/src/services/chat.service.js
+++ client/src/services/chat.service.js
@@ -1,2 +1,14 @@
 import { API_URL } from "./api.service";
 import axios from 'axios';
+
+export const fetchPost = async () => {
+    const response = await axios.get(`${API_URL}/chat`);
+    return response.data;
+};
+
+export const addPost = async (message) => {
+    const author = localStorage.getItem('name') + ' ' + localStorage.getItem('surname');
+    const timestamp = new Date().toLocaleString();
+    const response = await axios.post(`${API_URL}/chat`, { author, timestamp, message });
+    return true;
+};
---
+++ server/models/chat.model.js
@@ -0,0 +1,9 @@
+const post = {
+    author: "Jan Kowalski",
+    timestamp: "5/24/2024, 7:25:04 PM",
+    message: "Hej"
+};
+
+const posts = [post];
+
+module.exports = posts;
