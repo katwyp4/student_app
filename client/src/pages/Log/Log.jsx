1be4e95c5c28712a89767d0a612ddacc7c2cce59
Zuzansas <zuzanna.niciak@gmail.com>
2024-06-09 12:09:11 +0200

    Zmiana działania przycisku wstecz

--- client/src/pages/Log/Log.jsx
+++ client/src/pages/Log/Log.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { Form, Input, Button, notification, Typography , Tooltip} from "antd";
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Form, Input, Button, notification, Typography } from "antd";
 import { signin } from "../../services/log.service";
 import { EyeTwoTone, EyeInvisibleOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 
-
-
-export default function Log(){
-    const navigate = useNavigate(); // do przekierowania.
+export default function Log() {
+    const navigate = useNavigate();
     const [api, contextHolder] = notification.useNotification();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate("/menu", { replace: true });
+        } else {
+            window.history.pushState(null, document.title, window.location.href);
+            window.addEventListener('popstate', function (event) {
+                window.location.reload();
+            });
+        }
+
+        return () => {
+            window.removeEventListener('popstate', function (event) {
+                window.location.reload();
+            });
+        };
+    }, [navigate]);
+
     const notify = (type, title, message) => {
         api[type]({
             message: title,
@@ -20,22 +36,24 @@ export default function Log(){
     };
 
     const onFinish = async (values) => {
-        try{
-            if(!values.username && !values.password){ throw new Error('Brakuje danych.'); }
+        try {
+            if (!values.username && !values.password) { throw new Error('Brakuje danych.'); }
             const result = await signin(values.username, values.password);
-            if(result) {
+            if (result) {
+                localStorage.setItem("token", result.token); // Przechowywanie tokena
                 notify('success', 'Sukces!', `Operacja logowania przebiegła pomyślnie.`);
                 setTimeout(() => {
-                    navigate("/menu");
-                }, 1000); // Przekierowanie po 2 sekundach}
+                    navigate("/menu", { replace: true }); // Przekierowanie z zamianą historii
+                }, 1000);
+            } else {
+                throw new Error('Logowanie się nie powiodło spróbuj ponownie.');
             }
-            else { throw new Error('Logowanie się nie powiodło spróbuj ponownie.'); }
-        }catch(error){
+        } catch (error) {
             notify('error', 'Przykro nam :(', `Operacja logowania nie przebiegła pomyślnie`);
         }
     };
 
-    return(
+    return (
         <div style={{
             width: '100dvw',
             height: '100vh',
@@ -54,10 +72,10 @@ export default function Log(){
                 textAlign: 'center',
                 borderBottomLeftRadius: '50px',
                 borderBottomRightRadius: '50px',
-                zIndex: '1', // Wartość z-index dla tabeli
+                zIndex: '1',
                 position: 'relative'
             }}>
-                <img src={"logowikamp.png"} alt="Logo" style={{height: '70px', marginBottom: '20px'}}/>
+                <img src={"logowikamp.png"} alt="Logo" style={{ height: '70px', marginBottom: '20px' }} />
             </div>
             <div style={{
                 width: '275px',
@@ -68,11 +86,11 @@ export default function Log(){
                 marginTop: '50px',
                 padding: '30px',
                 margin: '-70px',
-                zIndex: '2',// Wartość z-index dla tabeli
-                position: 'relative'// Możesz usunąć tę właściwość, jeśli nie ma konfliktów z innymi elementami
+                zIndex: '2',
+                position: 'relative'
             }}>
-                <div style={{textAlign: 'center'}}>
-                    <Title level={3} style={{color: '#03045E', fontWeight: '300', marginTop: '-4px'}}>Logowanie</Title>
+                <div style={{ textAlign: 'center' }}>
+                    <Title level={3} style={{ color: '#03045E', fontWeight: '300', marginTop: '-4px' }}>Logowanie</Title>
                 </div>
                 <Form
                     name="login"
@@ -91,63 +109,57 @@ export default function Log(){
                     {contextHolder}
                     <Form.Item
                         name="username"
-                        label={<span style={{color: '#03045E', fontWeight: 'normal', fontSize: '8px',marginTop: '20px'}}>Nazwa użytkownika</span>}
+                        label={<span style={{ color: '#03045E', fontWeight: 'normal', fontSize: '8px', marginTop: '20px' }}>Nazwa użytkownika</span>}
                         labelAlign="left"
                         labelCol={{ span: 24 }}
                         rules={[
                             {
                                 required: true,
-                                message: <span style={{fontSize: '10px'}}>Proszę wpisać nazwę użytkownika!</span>
+                                message: <span style={{ fontSize: '10px' }}>Proszę wpisać nazwę użytkownika!</span>
                             },
                         ]}
-                        //style={{ marginTop: '20px' }} // Dodajemy odstęp poniżej pola input
                     >
                         <Input
-                            bordered={false} // Wyłączamy ramkę
-                            style={{ borderBottom: '1px solid #03045E', color: '#03045E', borderRadius:'0', borderBottomWidth: '1px', width: '270px', // Ustawiamy szerokość inputa na 100% rodzica
+                            bordered={false}
+                            style={{
+                                borderBottom: '1px solid #03045E', color: '#03045E', borderRadius: '0', borderBottomWidth: '1px', width: '270px',
                                 padding: '0',
                                 margin: '0',
                                 marginTop: '-10px',
-                            }} // Dodajemy linie pod polem input
+                            }}
                         />
                     </Form.Item>
                     <Form.Item
                         name="password"
-                        label={<span style={{color: '#03045E', fontWeight: 'normal', fontSize: '8px', marginTop: '20px'}}>Hasło</span>}
+                        label={<span style={{ color: '#03045E', fontWeight: 'normal', fontSize: '8px', marginTop: '20px' }}>Hasło</span>}
                         labelAlign="left"
                         labelCol={{ span: 24 }}
                         rules={[
                             {
                                 required: true,
-                                message:  <span style={{ fontSize: '10px' }}>Proszę wpisać hasło!</span>
+                                message: <span style={{ fontSize: '10px' }}>Proszę wpisać hasło!</span>
                             },
                         ]}
-                        //style={{ marginBottom: '10px', marginTop: '-70px' }} // Dodajemy odstęp poniżej pola input
                     >
                         <Input.Password
-                            bordered={false} // Wyłączamy ramkę
-                            style={{ borderBottom: '1px solid #03045E', color: '#03045E', borderRadius:'0', borderBottomWidth: '1px', width: '270px', // Ustawiamy szerokość inputa na 100% rodzica
+                            bordered={false}
+                            style={{
+                                borderBottom: '1px solid #03045E', color: '#03045E', borderRadius: '0', borderBottomWidth: '1px', width: '270px',
                                 padding: '0',
                                 margin: '0',
                                 marginTop: '-20px',
-                                //marginBottom: '5px'
                             }}
                             iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
                         />
-
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button" style={{width: '13dvw', backgroundColor: '#872220', borderRadius: '30px', marginBottom: '-40px'}}>Zaloguj się</Button>
+                        <Button type="primary" htmlType="submit" className="login-form-button" style={{ width: '13dvw', backgroundColor: '#872220', borderRadius: '30px', marginBottom: '-40px' }}>Zaloguj się</Button>
                     </Form.Item>
                     <Form.Item>
-
                         <Link to="/register" level={1} style={{ color: '#0077B6', fontSize: '10px' }}>Nie masz konta?</Link>
-
                     </Form.Item>
-
                 </Form>
             </div>
         </div>
-    )
-        ;
+    );
 }
