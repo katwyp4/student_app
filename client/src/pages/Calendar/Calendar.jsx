10a81d0e96b1b7bd53f680ed595c7c4ac4f8a2a3
HannaSauchuk <248664@edu.p.lodz.pl>
2024-06-15 13:31:15 +0200

    8. Dodanie funkcjonalności 'Utwórz'

--- client/src/pages/Calendar/Calendar.jsx
+++ client/src/pages/Calendar/Calendar.jsx
@@ -1,124 +1,72 @@
 import React, {useEffect, useState} from 'react';
-import {Badge, Calendar, ConfigProvider, Modal} from 'antd';
+import {Badge, Button, Calendar, Col, ConfigProvider, Divider, Input, Modal, Row,  TimePicker, message} from 'antd';
 import { BiArrowBack } from "react-icons/bi";
 import { useNavigate } from 'react-router-dom';
 import plPl from 'antd/es/locale/pl_PL'
 import dayjs from 'dayjs';
 import 'dayjs/locale/pl';
+import TextArea from "antd/es/input/TextArea";
 dayjs.locale('pl');
 
-let dayEvent = [
-    {
-        year: 2024,
-        month: 5,
-        day: 21,
-        type: "success",
-        content: "Dzień otwarty z firmą KILARGO",
-        details: "Dlaczego warto wziąć udział?\n" +
-            "\n" +
-            "Poznaj świat lodów Kilargo - Dowiedz się, jak powstają naszej wyjątkowe produkty i jakie technologie wykorzystujemy do produkcji.\n" +
-            "Ścieżki kariery - Zdobądź informacje o możliwościach rozwoju zawodowego w naszej firmie. Poznaj aktualne oferty pracy.\n" +
-            "Networking - Spotkaj się z przedstawicielem firmy Kilargo, zadaj pytania i zostaw swoje CV.\n" +
-            "Degustacja lodów - Skosztuj naszych pysznych lodów i przekonaj się, że nikt nie robi lepszych!\n" +
-            "Zapraszamy!\n" +
-            "Miejsce wydarzenia: wejście do budynku A2 Wydziału Biotechnologii i Nauk o Żywności, od strony ul. Stefanowskiego - w przypadku niepogody szukaj nas w środku!\n" +
-            "\nGodzina: 10:00 - 15:00\n" +
-            "Organizator: Kilargo\n"
-    },
-    {
-        year: 2024,
-        month: 5,
-        day: 21,
-        type: "success",
-        content: "Szkolenie \"Zarządzanie finansami w biznesie\"",
-        details: "Szkolenie  zostało tak zaprojektowane, aby dostarczyć uczestnikom wiedzy i umiejętności niezbędnych do efektywnego zarządzania finansami w sektorze biznesowym. Program skierowany jest do studentów, którzy chcą mądrze zarządzać finansami w swojej firmie.\n" +
-            "Celem szkolenia jest zapewnienie uczestnikom gruntownego zrozumienia kluczowych aspektów finansowych, niezbędnych do prowadzenia i rozwijania działalności gospodarczej. Uczestnicy nauczą się interpretować dane finansowe, przeprowadzać analizy finansowe, tworzyć skuteczne strategie budżetowania oraz planowania finansowego. Szkolenie obejmie również zagadnienia związane z zarządzaniem przepływami pieniężnymi i oceną ryzyka finansowego. Jego przedmiotem są także metody wyceny i zarządzania inwestycjami.\n" +
-            "Miejsce wydarzenia: Centrum Technologii Informatycznych, bud. B-19, Wólczańska 217/223\n" +
-            "Godzina: 14:00 - 16:00\n" +
-            "Organizator: Biuro Karier\n" +
-            "Cena wejścia: bezpłatne"
-    },
-    {
-        year: 2024,
-        month: 9,
-        day: 24,
-        type: "success",
-        content: "Szkolenie \"Zarządzanie finansami w biznesie\"",
-        details: "Szkolenie  zostało tak zaprojektowane, aby dostarczyć uczestnikom wiedzy i umiejętności niezbędnych do efektywnego zarządzania finansami w sektorze biznesowym. Program skierowany jest do studentów, którzy chcą mądrze zarządzać finansami w swojej firmie.\n" +
-            "Celem szkolenia jest zapewnienie uczestnikom gruntownego zrozumienia kluczowych aspektów finansowych, niezbędnych do prowadzenia i rozwijania działalności gospodarczej. Uczestnicy nauczą się interpretować dane finansowe, przeprowadzać analizy finansowe, tworzyć skuteczne strategie budżetowania oraz planowania finansowego. Szkolenie obejmie również zagadnienia związane z zarządzaniem przepływami pieniężnymi i oceną ryzyka finansowego. Jego przedmiotem są także metody wyceny i zarządzania inwestycjami.\n" +
-            "Miejsce wydarzenia: Centrum Technologii Informatycznych, bud. B-19, Wólczańska 217/223\n" +
-            "Godzina: 14:00 - 16:00\n" +
-            "Organizator: Biuro Karier\n" +
-            "Cena wejścia: bezpłatne"
-    },
-    {
-        year: 2024,
-        month: 9,
-        day: 25,
-        type: "success",
-        content: "Dzień otwarty z firmą KILARGO",
-        details: "Dlaczego warto wziąć udział?\n" +
-            "\n" +
-            "Poznaj świat lodów Kilargo - Dowiedz się, jak powstają naszej wyjątkowe produkty i jakie technologie wykorzystujemy do produkcji.\n" +
-            "Ścieżki kariery - Zdobądź informacje o możliwościach rozwoju zawodowego w naszej firmie. Poznaj aktualne oferty pracy.\n" +
-            "Networking - Spotkaj się z przedstawicielem firmy Kilargo, zadaj pytania i zostaw swoje CV.\n" +
-            "Degustacja lodów - Skosztuj naszych pysznych lodów i przekonaj się, że nikt nie robi lepszych!\n" +
-            "Zapraszamy!\n" +
-            "Miejsce wydarzenia: wejście do budynku A2 Wydziału Biotechnologii i Nauk o Żywności, od strony ul. Stefanowskiego - w przypadku niepogody szukaj nas w środku!\n" +
-            "\nGodzina: 10:00 - 15:00\n" +
-            "Organizator: Kilargo\n"
-    },
-    {
-        year: 2024,
-        month: 10,
-        day: 15,
-        type: "success",
-        content: "Dzień otwarty z firmą KILARGO",
-        details: "Dlaczego warto wziąć udział?\n" +
-            "\n" +
-            "Poznaj świat lodów Kilargo - Dowiedz się, jak powstają naszej wyjątkowe produkty i jakie technologie wykorzystujemy do produkcji.\n" +
-            "Ścieżki kariery - Zdobądź informacje o możliwościach rozwoju zawodowego w naszej firmie. Poznaj aktualne oferty pracy.\n" +
-            "Networking - Spotkaj się z przedstawicielem firmy Kilargo, zadaj pytania i zostaw swoje CV.\n" +
-            "Degustacja lodów - Skosztuj naszych pysznych lodów i przekonaj się, że nikt nie robi lepszych!\n" +
-            "Zapraszamy!\n" +
-            "Miejsce wydarzenia: wejście do budynku A2 Wydziału Biotechnologii i Nauk o Żywności, od strony ul. Stefanowskiego - w przypadku niepogody szukaj nas w środku!\n" +
-            "\nGodzina: 10:00 - 15:00\n" +
-            "Organizator: Kilargo\n"
-    },
-    {
-        year: 2023,
-        month: 9,
-        day: 15,
-        type: "success",
-        content: "Dzień otwarty z firmą KILARGO",
-        details: "Dlaczego warto wziąć udział?\n" +
-            "\n" +
-            "Poznaj świat lodów Kilargo - Dowiedz się, jak powstają naszej wyjątkowe produkty i jakie technologie wykorzystujemy do produkcji.\n" +
-            "Ścieżki kariery - Zdobądź informacje o możliwościach rozwoju zawodowego w naszej firmie. Poznaj aktualne oferty pracy.\n" +
-            "Networking - Spotkaj się z przedstawicielem firmy Kilargo, zadaj pytania i zostaw swoje CV.\n" +
-            "Degustacja lodów - Skosztuj naszych pysznych lodów i przekonaj się, że nikt nie robi lepszych!\n" +
-            "Zapraszamy!\n" +
-            "Miejsce wydarzenia: wejście do budynku A2 Wydziału Biotechnologii i Nauk o Żywności, od strony ul. Stefanowskiego - w przypadku niepogody szukaj nas w środku!\n" +
-            "\nGodzina: 10:00 - 15:00\n" +
-            "Organizator: Kilargo\n"
-    }
-]
-
+let isAdmin = true;
+let dayEvent = [];
+let eventKey = 1;
 function EventDetails({ details }) {
-    const formattedDetails = details.split('\n').map((line, index) => {
-        const colonIndex = line.indexOf(':');
-        if (colonIndex !== -1) {
-            const label = line.substring(0, colonIndex + 1);
-            const rest = line.substring(colonIndex + 1);
-            return <p key={index}><strong>{label}</strong>{rest}</p>;
-        }
-        return <p key={index}>{line}</p>;
-    });
-
     return (
         <div style={{ whiteSpace: 'pre-line' }}>
-            {formattedDetails}
+            {details.split('\n').map((line, index) => {
+                const [label, ...rest] = line.split(':');
+                return label && rest.length ? (
+                    <p key={index}>
+                        <strong>{label}:</strong> {rest.join(':')}
+                    </p>
+                ) : (
+                    <p key={index}>{line}</p>
+                );
+            })}
         </div>
+    );
+}
+
+function DisplayForm({ label, value, onChange, placeholder, disabled, showError, useTextArea, isTimePicker }) {
+    const format = 'HH:mm';
+    return (
+        <Row gutter={[16, 16]} style={{ marginBottom: '15px' }}>
+            <Col span={4} style={{ display: 'flex', alignItems: 'center' }}>
+                <p style={{ margin: 0 }}>{label}:</p>
+            </Col>
+            <Col span={20}>
+                {isTimePicker ? (
+                        <TimePicker
+                            minuteStep={15}
+                            format={format}
+                            value={value ? dayjs(value, format) : null}
+                            onChange={(time, timeString) => onChange(timeString)}
+                            style={{ marginBottom: '10px', borderColor: showError && !value ? 'red' : null }}
+                            disabled={disabled}
+                        />
+                ) :
+                useTextArea ? (
+                    <TextArea
+                        placeholder={placeholder}
+                        value={value}
+                        onChange={onChange}
+                        onPressEnter={(e) => e.preventDefault()}
+                        autoSize={{ minRows: 1, maxRows: 10 }}
+                        style={{ marginBottom: '10px', borderColor: showError && !value ? 'red' : null }}
+                        disabled={disabled}
+                    />
+                ) : (
+                    <Input
+                        placeholder={placeholder}
+                        value={value}
+                        onChange={onChange}
+                        style={{ marginBottom: '10px', borderColor: showError && !value ? 'red' : null }}
+                        disabled={disabled}
+                    />
+                )}
+            </Col>
+        </Row>
     );
 }
 export default function Calendar_fun(){
@@ -129,20 +77,76 @@ export default function Calendar_fun(){
     const [selectedMonthTxt, setSelectedMonthTxt] = useState(null);
     const [selectedYear, setSelectedYear] = useState(null);
 
+    const [description, setDescription] = useState('');
+    const [time, setTime] = useState('');
+    const [location, setLocation] = useState('');
+    const [content, setContent] = useState('');
+
+    const [showDescriptionInput, setShowDescriptionInput] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+    const handleCreateButtonClick = () => {
+        setShowDescriptionInput(true);
+        if(content && description && time && location)
+        {
+            if (isTimeInValidRange(time)) {
+                handleCreateEvent();
+            } else {
+                message.error('Niepoprawny zakres godzin.');
+            }
+        }
+        else
+        {
+            if (showDescriptionInput) {
+                setShowErrorMessage(true);
+            }
+        }
+    };
+    const handleCreateEvent = () => {
+        const newEvent = {
+            key: eventKey++,
+            year: selectedYear,
+            month: selectedMonth,
+            day: selectedDay,
+            type: "success",
+            content: content,
+            details: `Opis: ${description}\nGodzina: ${time}\nMiejsce: ${location}`
+        };
+        dayEvent.push(newEvent);
+        resetForm();
+        setIsModalOpen(false);
+        setShowDescriptionInput(false);
+        setShowErrorMessage(false);
+    };
+
+    const isTimeInValidRange = (time) => {
+        const format = 'HH:mm';
+        const minTime = dayjs('08:00', format);
+        const maxTime = dayjs('21:00', format);
+        const inputTime = dayjs(time, format);
+        return inputTime.isAfter(minTime) && inputTime.isBefore(maxTime);
+    };
+    const resetForm = () => {
+        setContent('');
+        setDescription('');
+        setTime('');
+        setLocation('');
+    };
     const handleOk = () => {
         setIsModalOpen(false);
     };
-
     const handleCancel = () => {
         setIsModalOpen(false);
+        setShowDescriptionInput(false);
+        setShowErrorMessage(false);
+        resetForm();
     };
-
     const onSelect = (value) => {
         if(selectedMonth===value.month()+1 && selectedYear === value.year()) setIsModalOpen(true);
         setSelectedDay(value.date());
         setSelectedMonth(value.month()+1);
         setSelectedMonthTxt(value.format('MMMM'));
         setSelectedYear(value.year());
+        setShowErrorMessage(false);
 
     };
 
@@ -153,7 +157,6 @@ export default function Calendar_fun(){
     const handleBackToMenu = () => {
         navigate('/menu');
     };
-
     const getListData = (value) => {
         const listData = dayEvent.filter(event =>
             event.year === value.year() &&
@@ -172,6 +175,7 @@ export default function Calendar_fun(){
             </ul>
         );
     };
+
     const cellRender = (current) => {
         return dateCellRender(current);
     };
@@ -204,9 +208,10 @@ export default function Calendar_fun(){
             position: sticky;
             top: 0;
             z-index: 1000;
-            height: 130px;
+            height: 150px;
             background-color: #882220;
             display: flex;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
             font-size: 35px;
@@ -230,6 +235,24 @@ export default function Calendar_fun(){
             overflow-y: auto;
         }
         
+        .week-days {
+            display: flex;
+            justify-content: space-around;
+            width: calc(100% - 40px);
+            font-size: 17px;
+            background-color: #882220;
+            color: #FFFFFF;
+            margin-top: 60px;
+        }
+        
+        .calendar-content {
+            height: calc(100vh - 160px);
+            overflow-y: auto;
+        }
+        
+        .ant-picker-content th {
+            display:none;
+        }
         
       `}</style>
             <div className="calendar-header">
@@ -237,16 +260,83 @@ export default function Calendar_fun(){
                     <BiArrowBack/>
                 </div>
                 Kalendarz
+                <div className="week-days">
+                    <span>Pn</span>
+                    <span>Wt</span>
+                    <span>Śr</span>
+                    <span>Cz</span>
+                    <span>Pt</span>
+                    <span>Sb</span>
+                    <span>Nd</span>
+                </div>
             </div>
+
             <div className="calendar-content">
                 <Calendar cellRender={cellRender} onSelect={onSelect}/>
             </div>
             <Modal title={selectedDay && selectedMonthTxt ? `${selectedDay} ${selectedMonthTxt}` : 'Basic Modal'}
-                   open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                   open={isModalOpen} onOk={handleOk} onCancel={handleCancel}
+
+                   footer={isAdmin ? [
+                       <Button key="cancel" onClick={handleCancel}>
+                           Anuluj
+                       </Button>,
+                       <Button key="create" type="primary" onClick={handleCreateButtonClick}>
+                           Utworz
+                       </Button>
+                   ] : null}
+            >
+                {showDescriptionInput && (
+                    <>
+                        <DisplayForm
+                            label="Nagłówek"
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            placeholder="Nagłówek"
+                            disabled={!showDescriptionInput}
+                            showError={showErrorMessage}
+                            useTextArea={true}
+                            isTimePicker={false}
+                        />
+                        <DisplayForm
+                            label="Opis"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            placeholder="Opis"
+                            disabled={!showDescriptionInput}
+                            showError={showErrorMessage}
+                            useTextArea={true}
+                            isTimePicker={false}
+                        />
+                        <DisplayForm
+                            label="Godzina"
+                            value={time}
+                            onChange={setTime}
+                            disabled={!showDescriptionInput}
+                            showError={showErrorMessage}
+                            useTextArea={false}
+                            isTimePicker={true}
+
+                        />
+                        <DisplayForm
+                            label="Miejsce"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                            placeholder="Miejsce"
+                            disabled={!showDescriptionInput}
+                            showError={showErrorMessage}
+                            useTextArea={false}
+                            isTimePicker={false}
+                        />
+
+                    </>
+                )}
+                {showErrorMessage && <p style={{ color: 'red' }}>Nie wszystkie pola zostały wypełnione</p>}
                 {getListData(dayjs(`${selectedYear}-${selectedMonth}-${selectedDay}`)).map((event, index) => (
                     <div key={index}>
                         <h2>{event.content}</h2>
                         <EventDetails details={event.details}/>
+                        {index!==getListData(dayjs(`${selectedYear}-${selectedMonth}-${selectedDay}`)).length-1 && <Divider/>}
                     </div>
                 ))}
             </Modal>
