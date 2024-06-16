2e9a3241ac63e8b71a54f6775459c297445168e9
Denys Puha <248663@edu.p.lodz.pl>
2024-06-17 01:10:56 +0200

    Stworzony back-end dla planu zajęć

--- client/src/services/schedule.service.js
+++ client/src/services/schedule.service.js
@@ -1,2 +1,17 @@
 import { API_URL } from "./api.service";
 import axios from 'axios';
+
+export const fetchEvent = async () => {
+    const response = await axios.get(`${API_URL}/schedule`);
+    return response.data;
+};
+
+export const addEvent = async (event) => {
+    await axios.post(`${API_URL}/schedule`, event);
+    return true;
+};
+
+export const deleteEvent = async (keyToDelete) => {
+    await axios.delete(`${API_URL}/schedule/${keyToDelete}`);
+    return true;
+};
--- server/routes/schedule.route.js
+++ server/routes/schedule.route.js
@@ -1,4 +1,9 @@
 //post  i get
 var express = require('express');
+const chatController = require("../controllers/schedule.controller");
 var router = express.Router();
+
+router.get('/', chatController.fetch);
+router.post('/', chatController.add);
+router.delete('/:key', chatController.destroy);
 module.exports = router;
